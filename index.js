b239d87924b5fb1a84c5703c83d3ec3fe8becd24
Prabhash8853 <prabhash.singh.cs.2015@miet.ac.in>
2019-07-03 22:16:46 +0530

    get put post delete working done

--- index.js
+++ index.js
@@ -1,12 +1,85 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const hostname = 'localhost';
 const port = 3000;
+
 const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',( req, res, next ) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    next(); // it will continue on to look for additional matches for queries in the endpoints
+})
+//first the above function will work then below one will execute
+
+app.get('/dishes', (req,res,next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req,res,next) => {
+    //if we get post request then above app.all will execute first and then due to
+    //next() this function of post will execute
+
+    res.end('Will add the dish: ' + req.body.name + 'with details: ' + req.body.description);
+});
+
+
+app.put('/dishes', (req,res,next) => {
+    //if we get put or update request then above app.all will execute first and then due to
+    //next() this function of post will execute
+    res.statusCode = 403;
+    res.end('PUT operation not supported on dishes');
+});
+
+
+app.delete('/dishes', (req,res,next) => {
+    //if we get delete request then above app.all will execute first and then due to
+    //next() this function of post will execute
+
+    res.end('Deleting all the dishes!');
+});
+
+
+
+
+// function for params
+
+
+
+
+
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send the dishes to you !' + req.params.dishId);
+});
+
+
+app.post('/dishes/:dishId', (req,res,next) => {
+    res.end('Not Supported Post operation');
+});
+
+
+app.put('/dishes/:dishId', (req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will Update the dish: '+ req.params.dishId);
+});
+
+
+app.delete('/dishes/:dishId', (req,res,next) => {
+    res.end('Deleting dish: '+req.params.dishId);
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
 
 app.use(( req, res, next ) => {
-    console.log(req.headers);
+    
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>');
